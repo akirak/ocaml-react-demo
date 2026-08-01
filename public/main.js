8c1b535a8451b9a8d037cf089f1bd97757d6e6bc
Akira Komamura <akira.komamura@gmail.com>
2026-08-01 11:58:22 +0900

    Don't generate js files in the same directory

---
+++ public/main.js
@@ -0,0 +1,2 @@
+// Copied from https://github.com/lessp/react-ml-minimal-template/blob/main/public/index.html
+import "../_build/default/src/src/App.js";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    nodeResolve(),
+  ],
+  root: "public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  },
 })
