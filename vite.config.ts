9f49411e8aab038ae413a6fd5a3b9c1bf3077cc9
Akira Komamura <akira.komamura@gmail.com>
2026-08-01 12:23:21 +0900

    Remove the unnecessary plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import browserslist from 'browserslist';
 import {browserslistToTargets} from 'lightningcss';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    nodeResolve(),
-  ],
   css: {
     transformer: 'lightningcss',
     lightningcss: {
